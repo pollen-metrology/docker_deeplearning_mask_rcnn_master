331379fbcbf712fb6c4c8b1d5854f7d2ed2718ce
erichardpollen <56431194+erichardpollen@users.noreply.github.com>
2019-11-08 15:52:57 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# docker run -it -d --gpus all --name "deeplearning_mask_rcnn_master" -v /home/docker/deeplearning_mask_rcnn_master/jenkins_agent/ws:/home/jenkins pollenm/docker_deeplearning_mask_rcnn_master
+
+docker run \
+  -e JENKINS_URL=https://jenkins.pollen-metrology.com/ \
+  -e JENKINS_SECRET=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX \
+  -e JENKINS_AGENT_WORKDIR=/home/jenkins \
+  -e JENKINS_NAME=Deep_Learning_mask_rcnn_master \
+  -it \
+  -d \
+  --gpus all \
+  --name "deeplearning_mask_rcnn_master" \
+  --restart always \
+  -v /home/docker/deeplearning_mask_rcnn_master/jenkins_agent/ws:/home/jenkins \
+  -v /home/deeplearning_script/:/home/scripts/ \
+  pollenm/docker_deeplearning_mask_rcnn_master
